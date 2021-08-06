e354114c45319be871c762bbe9c22f7170125c22
AnakinLalwani <72091898+AnakinLalwani@users.noreply.github.com>
2021-08-06 06:10:46 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -17,8 +17,31 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 500, 400);
+}
+function modelLoaded() {
+    console.log("Model is Loaded");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log(scoreLeftWrist);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        console.log(scoreRightWrist);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("left x: " + leftWristX + " left y: " + leftWristY);
+        console.log("right x: " + rightWristX + " right y: " + rightWristY);
+    }
 }
